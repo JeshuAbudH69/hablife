461f8cebb5c01cb9fbf86e02b63a910e26c85543
JeshuAbudH69 <42950452+JeshuAbudH69@users.noreply.github.com>
2018-09-03 22:27:10 -0400

    Add files via upload

---
+++ register.php
@@ -0,0 +1,97 @@
+<?php 
+  require 'init.php';
+  include 'anotherdata/header.php';
+    onSES();
+  include 'kernel/core_users.php';
+?>
+<div id="contenedor-topstorys">
+
+<div id="contenedor-imagenes">
+
+<div id="general"><a href="#"><div id="img-general"><div id="general-texto">Noticia | Inauguración de Hablife</div></div></div></a>
+
+<div id="img"><a href="#"><div id="img-1"><div id="texto-1">Eventos | Ejemplo</div></div></div></a>
+<div id="img-2-2"><a href="#"><div id="img-2"><div id="texto-2">Guias | Ejemplo</div></div></div></a>
+
+<div id="img-3-3"><a href="#"><div id="img-3"><div id="texto-3">Eventos | Ejemplo</div></div></div></a>
+<div id="img-4-4"><a href="#"><div id="img-4"><div id="texto-4">Noticias | Ejemplo</div></div></div></a>
+
+<div class="corrector"></div>
+
+</div>
+
+<div class="corrector"></div>
+
+</div>
+
+<div id="linea-decoracion"></div>
+
+<div id="cuerpo-web">
+
+<div id="contenedor-box">
+
+<div id="box-1"><div id="box-titulo-1"><div id="img-b1"><img src="img/icon-6.png" /></div>Últimas Placas</div><div id="texto-box-1">
+
+<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/DE740.gif"/></div>
+<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FR672.gif"/></div>
+<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FR675.gif"/></div>
+<div id="cuadro-badge-3"><img src="http://images.habbo.com/c_images/album1584/FR676.gif"/></div>
+<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/UK499.gif"/></div>
+<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FI175.gif"/></div>
+<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FR677.gif"/></div>
+<div id="cuadro-badge-3"><img src="http://images.habbo.com/c_images/album1584/FR673.gif"/></div>
+
+</div>
+
+<div class="corrector"></div>
+
+</div>
+
+<div id="box-2"><div id="box-titulo-2"><div id="img-b2"><img src="img/pu.png" /></div>Salas Oficiales</div><div id="texto-box-2">
+
+<a href="https://www.habbo.es/room/107625710"><div id="sala-1"><div id="img-sala-1"><img src="img/room_icon_3.gif"/></div><b>»[HL]» Discoteca Pública</b><br>Dueñ@: @.Lince.@</div></a>
+
+<a href="https://www.habbo.es/room/106495843"><div id="sala-1"><div id="img-sala-1"><img src="img/room_icon_3.gif"/></div><b>»[HL]» Central de Juegos</b><br>Dueñ@: @.Lince.@</div></a>
+
+<a href="https://www.habbo.es/room/107540749"><div id="sala-1"><div id="img-sala-1"><img src="img/room_icon_3.gif"/></div><b>»[HL]» Help Center</b><br>Dueñ@: @.Lince.@</div></a>
+
+<div class="corrector"></div>
+
+</div></div>
+<div id="box-4"><div id="box-titulo-4"><div id="img-b2"><img src="img/fb.png" /></div>Facebook</div><div id="texto-box-4">
+<div class="fb-page" data-href="https://www.facebook.com/pages/ESHablife/100504550295621?fref=ts" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/ESHablife/100504550295621?fref=ts"><a href="https://www.facebook.com/pages/ESHablife/100504550295621?fref=ts">ESHablife</a></blockquote></div></div>
+<div class="corrector"></div>
+
+</div></div>
+
+
+<div id="box-3"><div id="box-titulo-3"><div id="img-b3"><img src="img/twitter.png" /></div>Nuestro Twitter</div><div id="texto-box-3"><a class="twitter-timeline" href="https://twitter.com/Linzace" data-widget-id="607307225896513536">Tweets por el @Linzace.</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div></div>
+
+</div>
+
+<div id="contenedor-noticia"><div id="noticia-titulo"><div id="img-noticia"><img src="img/noti.png" /></div>Registrate en <?php echo BN('title') ?></div><div id="texto-noticia">
+	<center>
+		<p id="titulo">Completa todo el formulario como se te pide</p>
+
+		    <form class="Addsled" method="POST">
+         	<input type="text" name="user" placeholder="Nombre de usuario (45 caracteres)" maxlength="45">
+         	<input type="text" id="nombre" name="name" placeholder="Nombre completo" maxlength="125">
+         	<input type="password" id="pass" name="pass" placeholder="Contrase&ntilde;a" maxlength="32">
+         	<input type="password" id="pass" name="pas2" placeholder="Repite la contrase&ntilde;a" maxlength="32">
+         	<input type="mail" id="mail" name="emal" placeholder="Correo electr&oacute;nico" maxlength="350">
+         	<input type="text" id="habbo" name="habo" placeholder="Usuario en HabboES" maxlength="45">
+			<input type="submit" name="user_register" value="Crear cuenta">
+		</form>
+	</section>
+	</center>
+</div>
+</div>
+</div>
+
+<div class="corrector"></div>
+
+</div></div>
+
+
+<?php include('anotherdata/footer.php'); ?>
