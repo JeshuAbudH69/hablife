15d350ea11b75dad0eb7cd3731020a3ee8685a1f
JeshuAbudH69 <42950452+JeshuAbudH69@users.noreply.github.com>
2018-09-03 22:17:52 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -27,48 +27,6 @@
 
 <div id="cuerpo-web">
 
-<div id="contenedor-box">
-
-<div id="box-1"><div id="box-titulo-1"><div id="img-b1"><img src="img/icon-6.png" /></div>Últimas Placas</div><div id="texto-box-1">
-
-<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/DE740.gif"/></div>
-<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FR672.gif"/></div>
-<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FR675.gif"/></div>
-<div id="cuadro-badge-3"><img src="http://images.habbo.com/c_images/album1584/FR676.gif"/></div>
-<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/UK499.gif"/></div>
-<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FI175.gif"/></div>
-<div id="cuadro-badge"><img src="http://images.habbo.com/c_images/album1584/FR677.gif"/></div>
-<div id="cuadro-badge-3"><img src="http://images.habbo.com/c_images/album1584/FR673.gif"/></div>
-
-</div>
-
-<div class="corrector"></div>
-
-</div>
-
-<div id="box-2"><div id="box-titulo-2"><div id="img-b2"><img src="img/pu.png" /></div>Salas Oficiales</div><div id="texto-box-2">
-
-<a href="https://www.habbo.es/room/107625710"><div id="sala-1"><div id="img-sala-1"><img src="img/room_icon_3.gif"/></div><b>»[HL]» Discoteca Pública</b><br>Dueñ@: @.Lince.@</div></a>
-
-<a href="https://www.habbo.es/room/106495843"><div id="sala-1"><div id="img-sala-1"><img src="img/room_icon_3.gif"/></div><b>»[HL]» Central de Juegos</b><br>Dueñ@: @.Lince.@</div></a>
-
-<a href="https://www.habbo.es/room/107540749"><div id="sala-1"><div id="img-sala-1"><img src="img/room_icon_3.gif"/></div><b>»[HL]» Help Center</b><br>Dueñ@: @.Lince.@</div></a>
-
-<div class="corrector"></div>
-
-</div></div>
-<div id="box-4"><div id="box-titulo-4"><div id="img-b2"><img src="img/fb.png" /></div>Facebook</div><div id="texto-box-4">
-<div class="fb-page" data-href="https://www.facebook.com/pages/ESHablife/100504550295621?fref=ts" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/ESHablife/100504550295621?fref=ts"><a href="https://www.facebook.com/pages/ESHablife/100504550295621?fref=ts">ESHablife</a></blockquote></div></div>
-<div class="corrector"></div>
-
-</div></div>
-
-
-<div id="box-3"><div id="box-titulo-3"><div id="img-b3"><img src="img/twitter.png" /></div>Nuestro Twitter</div><div id="texto-box-3"><a class="twitter-timeline" href="https://twitter.com/Linzace" data-widget-id="607307225896513536">Tweets por el @Linzace.</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div></div>
-
-</div>
-
 <div id="contenedor-noticia"><div id="noticia-titulo"><div id="img-noticia"><img src="img/noti.png" /></div>Noticias de Habbo</div><div id="texto-noticia">
 <div id="noticia">
 <div style="position: absolute; z-index: 1; margin-top: -1px; margin-left: -1px;"><img alt="" src="img/pruebagoku.jpeg" style="border: none;"></div>
@@ -220,6 +178,7 @@
 </div></div>
 
 <?php 
+ include ('anotherdata/left.tpl');
   include ('anotherdata/right.tpl');
   include ('anotherdata/footer.php');
 ?>
